5f7a21c086c0cecacfc44cb5dda46ea02135f0c6
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-09-26 17:22:43 -0300

    added logger

--- src/main/java/com/facu/altisima/AltisimaApplication.java
+++ src/main/java/com/facu/altisima/AltisimaApplication.java
@@ -1,5 +1,7 @@
 package com.facu.altisima;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +11,8 @@ import org.springframework.web.filter.CommonsRequestLoggingFilter;
 public class AltisimaApplication {
 
 	public static void main(String[] args) {
+		Logger log = LoggerFactory.getLogger(AltisimaApplication.class);
+		log.info("Starting Altisima Application");
 		SpringApplication.run(AltisimaApplication.class, args);
 	}
 
