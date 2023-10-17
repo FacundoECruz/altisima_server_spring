3e2196a95c89ec7a677317855d91d35a3153186c
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-10-17 11:12:48 -0300

    created associate user dto

---
+++ src/main/java/com/facu/altisima/controller/dto/AssociateUserDto.java
@@ -0,0 +1,122 @@
+package com.facu.altisima.controller.dto;
+
+import com.facu.altisima.controller.dto.legacyDtos.CreateUserDto;
+import com.facu.altisima.model.User;
+import com.facu.altisima.service.utils.IdGenerator;
+import com.facu.altisima.service.utils.UUIDGenerator;
+
+import java.util.Objects;
+
+public class AssociateUserDto {
+    public static final String EMPTY_FIELD_MSG = "Completar todos los campos";
+    public static final String DEFAULT_IMAGE_URL = "https://play-lh.googleusercontent.com/rX_nOuUDijsV_NnWZP9JgYTsFpxn5y7qCqDxFIpZ-BqiJu8un7UbdSgVTZSrJuzAlQ";    IdGenerator idGenerator = new UUIDGenerator();
+    private String username;
+    private String email;
+    private PasswordDto password;
+    private String image;
+
+    private String toAssociate;
+
+    public AssociateUserDto(
+            String username,
+            String email,
+            String password,
+            String image,
+            String toAssociate) {
+        this.username = username;
+        this.email = email;
+        this.password = new PasswordDto(password);
+        this.image = image;
+        this.toAssociate = toAssociate;
+    }
+
+    public void validate() {
+        password.validate();
+        if (username.equals("") || email.equals("")) {
+            throw new RuntimeException(EMPTY_FIELD_MSG);
+        } else if (!email.contains("@")) {
+            throw new RuntimeException(invalidEmailMsg(email));
+        }
+    }
+
+    public static String invalidEmailMsg(String email) {
+        return "El email no es valido: " + email;
+    }
+
+    public User toDomain() {
+        validate();
+        return new User(idGenerator.generate(),
+                username,
+                email,
+                getDefaultImage(),
+                password.getValue(),
+                0);
+    }
+
+    private String getDefaultImage() {
+        return image.equals("") ? DEFAULT_IMAGE_URL : image;
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password.getValue();
+    }
+
+    public void setPassword(String password) {
+        this.password = new PasswordDto(password);
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getToAssociate() {
+        return toAssociate;
+    }
+
+    public void setToAssociate(String toAssociate) {
+        this.toAssociate = toAssociate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AssociateUserDto that = (AssociateUserDto) o;
+        return Objects.equals(username, that.username)
+                && Objects.equals(email, that.email)
+                && Objects.equals(password, that.password)
+                && Objects.equals(image, that.image)
+                && Objects.equals(toAssociate, that.toAssociate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(
+                username,
+                email,
+                password,
+                image,
+                toAssociate);
+    }
+}
